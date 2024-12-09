01008d45bff472bae2a50d5c7a9c9b8aa9dd8ca2
Matthew Millard <matthewmillard@Matthews-MacBook-Pro.local>
2024-12-09 01:16:11 -0500

    add day 07 part 1

--- day_07/recursion.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function countDown(number) {
-    if (number < 0)
-        return;
-    console.log(number);
-    countDown(number - 1);
-}
-countDown(10);
--- day_07/recursion.ts
+++
@@ -1,9 +0,0 @@
-function countDown(number: number) {
-  if (number < 0) return;
-
-  console.log(number);
-
-  countDown(number - 1);
-}
-
-countDown(10);
